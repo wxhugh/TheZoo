b442759b2c7e466f290c29c4fa589ffbc58b09d5
Chris Caragianis <chriscaragianis@gmail.com>
2016-04-19 10:39:06 -0400

    Set up abstract Animal class

---
+++ Animal.java
@@ -0,0 +1,9 @@
+public abstract class Animal {
+    
+  private int age; //in days
+  private boolean alive;
+  private double weight;
+  private double hunger; //0 to 1;
+  
+  public abstract void eat();
+} 
---
+++ Zoo.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Zoo {
+
+    private int animalCount;
+    private List<Animal> animals;
+    
+    public void rollCall() {    
+        //TODO: log info on
+        //each animal to the 
+        //console.
+    }    
+}
